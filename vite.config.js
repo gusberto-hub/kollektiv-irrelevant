331fa974eff52d85be7ca092a35ad76d3232285a
gdiguglielmo <gusbertus@gmail.com>
2024-02-19 09:09:53 +0100

    adding scss

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,15 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	css: {
+		preprocessorOptions: {
+			scss: {
+				additionalData: `
+				@use './src/lib/scss/variables' as *;
+				@use './src/lib/scss/mixins' as *;
+				`
+			}
+		}
+	}
 });
