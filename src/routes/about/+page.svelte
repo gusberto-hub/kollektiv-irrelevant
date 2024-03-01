33371f3f7e9ddcf6f0d58641f64ba6005f55e7f5
gdiguglielmo <gusbertus@gmail.com>
2024-03-01 23:22:43 +0100

    about page styling

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -2,38 +2,36 @@
 	export let data;
 </script>
 
-<div class="grid gap-4 pt-40">
-	<div class="shared-grid gap-4">
-		<h1 class="font-extrabold uppercase text-2xl md:text-4xl hyphens-auto col-start-4 col-end-13">
-			das Kollektiv
-		</h1>
-		<p class="col-start-4 col-end-13">
+<section id="about-page">
+	<div class="about-hero-container">
+		<h1 class="title">das Kollektiv</h1>
+		<p class="paragraph">
 			{data.page.pageDescription}
 		</p>
 		<img
 			src="https://uploads-ssl.webflow.com/63419ff88f1eec1858f57229/63595827b22cf279b2eda3d9_Gr%C3%BCnder.jpg"
 			alt="the kollektivs founders"
-			class="col-start-4 col-end-13"
+			class="image"
 		/>
-		<div class="col-start-4 col-end-13">
-			<h1 class="font-extrabold uppercase text-2xl md:text-4xl">die Irrelevanten</h1>
-			<div class="sm:grid grid-cols-2 gap-4">
-				{#each data.teamMembersConnection.edges as member}
-					<div class="grid grid-cols-2 border-t-4 border-white gap-4">
-						<div class="aspect-square">
-							<img
-								class="w-full h-full object-cover"
-								src={member.node.picture.url}
-								alt={member.node.name + ' ' + member.node.role}
-							/>
-						</div>
-						<div class="py-4">
-							<p class="font-bold text-2xl text-primary">{member.node.name}</p>
-							<p>{member.node.role}</p>
-						</div>
+	</div>
+	<div class="member-container">
+		<h1 class="member-title">die Irrelevanten</h1>
+		<div class="member-wrapper">
+			{#each data.teamMembersConnection.edges as member}
+				<div class="member-card">
+					<div class="member-card__image-container">
+						<img
+							class=""
+							src={member.node.picture.url}
+							alt={member.node.name + ' ' + member.node.role}
+						/>
 					</div>
-				{/each}
-			</div>
+					<div class="member-card__details">
+						<p class="name">{member.node.name}</p>
+						<p class="role">{member.node.role}</p>
+					</div>
+				</div>
+			{/each}
 		</div>
 	</div>
-</div>
+</section>
