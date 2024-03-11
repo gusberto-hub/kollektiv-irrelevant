2ff65f4cbd172478d089cbc7fede2bc62f5586c4
gdiguglielmo <gusbertus@gmail.com>
2024-03-11 08:02:30 +0100

    WIP add trail effect when mouse moving

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,43 @@
 	import '../lib/scss/global.scss';
 	import Footer from './_components/Footer.svelte';
 	import Navbar from './_components/Navbar.svelte';
+	import P5 from 'p5-svelte';
+
+	let frameCounter = 0;
+	let trail = [];
+
+	const sketch = (p5) => {
+		p5.setup = () => {
+			p5.createCanvas(p5.windowWidth, p5.windowHeight);
+			p5.noStroke();
+		};
+
+		p5.draw = () => {
+			p5.background('#0e0e1a');
+
+			p5.fill('#fb0089');
+
+			trail.push(p5.createVector(p5.mouseX, p5.mouseY));
+
+			// if (p5.mouseX !== p5.pmouseX || p5.mouseY !== p5.pmouseY) {
+			// 	trail.push(p5.createVector(p5.mouseX, p5.mouseY));
+			// } else {
+			// 	trail.shift();
+			// }
+
+			if (trail.length > 80) {
+				trail.shift();
+			}
+
+			for (let i = 0; i < trail.length; i++) {
+				const curr = trail[i];
+				// const currD = i < 50 ? p5.map(i, 0, 50, 0, 99) : p5.map(i, 50, 100, 99, 0);
+				p5.circle(curr.x, curr.y, 60);
+			}
+
+			// frameCounter < 500 ? frameCounter++ : (frameCounter = 0);
+		};
+	};
 </script>
 
 <svelte:head>
@@ -9,6 +46,9 @@
 </svelte:head>
 
 <div class="main-container">
+	<div class="canvas">
+		<P5 {sketch} />
+	</div>
 	<Navbar />
 	<main class="">
 		<slot />
@@ -19,4 +59,11 @@
 </div>
 
 <style>
+	.canvas {
+		position: fixed;
+		inset: 0;
+		z-index: -1;
+		/* filter: blur(20px); */
+		/* border: 2px solid red; */
+	}
 </style>
